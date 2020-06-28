af77a4d243ccc8e3a5b183587a07faa8955da565
Gourab Sarkar <GourabIX@users.noreply.github.com>
2020-06-29 05:01:27 +0530

    Adds functionality to Style 3 component (Text-only carousel).
    Text-only carousel is now fully functional with custom icons and custom indicators.
    There might be an issue with the Bootstrap carousels: they don't start automatically until the user click.

--- src/app/style3/style3.component.ts
+++ src/app/style3/style3.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-style3',
-  templateUrl: './style3.component.html',
-  styleUrls: ['./style3.component.css']
+  selector: "app-style3",
+  templateUrl: "./style3.component.html",
+  styleUrls: ["./style3.component.css"],
 })
 export class Style3Component implements OnInit {
+  url: string = "https://www.wipo.int/wipo-match/en/";
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
